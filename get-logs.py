bcbb14b2dbfac65465eb8afb080098ed73edba9c
Bruno Meneguele <bruno.meneguele@smartgreen.net>
2016-01-29 17:13:58 -0200

    CHANGE get-logs: alterado modo de buscar datas.
    Anteriormente era feita uma busca pelas linhas que continham os limites dos
    dados requeridos depois uma leitura entre estes limites era feita a fim de obter
    as linhas intermediárias. Porém, a busca era feita sobre strings. Agora a busca
    é feita sobre inteiros (convertendo as datas para um formato apropriado:
    2016-01-01 para 20160101) tornando a busca por expressões regulares mais rápida.
    
    Com isso, a cada match de data neste formato que estiver entre o intervalo dos
    limites a linha é inteira copiada para a memória e posteriormente para um
    arquivo.

--- get-logs.py
+++ get-logs.py
@@ -1,7 +1,5 @@
 import os
-import sys
 import re
-import mmap
 import socket
 import tarfile
 import argparse
@@ -14,12 +12,18 @@ class LogParser(object):
     def __init__(self, date_range, time_range, log_dir, log_prefix,
                  output_name):
         """Instantiates the log parser and parser all log paths requested.
+        It also transforms date format from '2015-02-01,2016-01-01' to a list of
+        integers [20150201, 20160101] which it's easier and faster to compare.
         """
 
-        self.date_range = date_range.split(',')
+        self.date_range = [int(''.join(date.split('-', 2)))
+                           for date in date_range.split(',')]
         self.time_range = time_range.split(',')
         self.log_dir = log_dir
         self.log_files = log_prefix.split(',')
+
+        print "INFO: start date is {0}, end date is {1}".format(
+                *self.date_range)
 
         if output_name == '':
             output_name = ''.join([self._sgcon_get_hostname(), '-logs.tar.gz'])
@@ -31,7 +35,6 @@ class LogParser(object):
         for lfile in self.log_files:
             if lfile.count('*'):
                 lpre, _ = lfile.split('*', 1)
-                print 'lpre: ', lpre
                 self.log_files.remove(lfile)
 
                 for f in os.listdir(self.log_dir):
@@ -41,76 +44,65 @@ class LogParser(object):
                         continue
 
                     self.log_files.extend(matches.groups())
-
+                    self.log_files = sorted(self.log_files, reverse=True)
 
         # Join all requested files to the log_path. i.e. /var/log/sgcon +
         # suiteCore = /var/log/sgcon/suiteCore
         self.log_paths = [os.path.join(self.log_dir, lfile)
-                         for lfile in self.log_files]
+                          for lfile in self.log_files]
 
         print 'Log files:'
         print self.log_paths
-        print 'Output file: ', self.output_name
-        self.get_logs()
 
+    @staticmethod
     def _sgcon_get_hostname(self):
         return socket.gethostname()
 
     # Log line example:
-    # 2016-01-25 11:13:21,330 PowerMonitor INFO MainThread Service stopped.
     def get_logs(self):
-        """ Get all log lines among date and time range.
+        """ Get all log lines among date and time range following the format:
+
+        "2016-01-25 11:13:21,330 PowerMonitor INFO MainThread Service stopped."
         """
 
-        start_lnum = end_lnum = 0
-        pattern = [r'({}.*)'.format(date) for date in self.date_range]
+        re_date = re.compile(r'(\d{4}-\d{2}-\d{2})')
 
-        for log_file in self.log_paths:
-            with open(os.path.realpath(log_file), 'r') as log:
-                start_lnum, end_lnum = self._get_lnums(log, pattern)
+        for log_path in self.log_paths:
+            m_lines = []
 
-                if start_lnum != 0 and end_lnum != 0:
-                    print start_lnum, end_lnum
-                    break
-                else:
-                    # TODO: it's possible the end_lnum isn't in the same file
-                    if start_lnum == 0:
-                        print >> sys.stderr, "ERROR: start_lnum was not found."
-                    elif end_lnum == 0:
-                        print >> sys.stderr, "ERROR: end_lnum was not found."
+            with open(os.path.realpath(log_path)) as log:
+                for line in log.readlines():
+                    m = re_date.match(line)
 
-    def _get_lnums(self, f, pattern):
-        """ Find the line numbers of the start line and the end line to the
-        requested date and time range.
-        """
+                    if m:
+                        m_value = int(''.join(m.group(0).split('-', 2)))
 
-        start_lnum = end_lnum = 0
+                        if self.date_range[0] <= m_value <= self.date_range[1]:
+                            m_lines.append(line)
 
-        # First try to find the start line number, then search for the end line.
-        for lnum, line in enumerate(f.readlines(), 1):
-            if start_lnum == end_lnum == 0:
-                if re.search(pattern[0], line):
-                    start_lnum = lnum
+                bkp_file_path = os.path.dirname(os.path.realpath(
+                        self.output_name))
 
-            if start_lnum != 0 and end_lnum == 0:
-                if re.search(pattern[1], line):
-                    end_lnum = lnum
-
-        return (start_lnum, end_lnum)
+            if m_lines:
+                with open('{0}/{1}'.format(bkp_file_path, os.path.basename(
+                        log_path)), 'w+') as bkp_file:
+                    map(bkp_file.write, m_lines)
 
     def compress_logs(self):
         pass
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Get all SG Concentrador logs")
+    parser = argparse.ArgumentParser(description="Get any log based in the\
+                                     Python 'logging' module from a date and time\
+                                     range")
 
     parser.add_argument('-d',
                         action='store',
                         dest='date_range',
                         type=str,
                         default='*',
-                        help="Set the date range from the begining to the end\
-                        separated by commas (,). Example: 01-01-2010,31-12-2015"
+                        help="Set the date range from the begining to the end.\
+                        Example: 01-01-2010,31-12-2015"
                         )
 
     parser.add_argument('-t',
@@ -118,8 +110,8 @@ if __name__ == '__main__':
                         dest='time_range',
                         type=str,
                         default='*',
-                        help="Set the time range from the begining to the end\
-                        separated by commas (,). Example: 00:00:00,23:59:59"
+                        help="Set the time range from the begining to the end.\
+                        Example: 00:00:00,23:59:59"
                         )
 
     parser.add_argument('-p',
@@ -127,8 +119,8 @@ if __name__ == '__main__':
                         dest='log_prefix',
                         type=str,
                         default='*',
-                        help="Get logs based on its name prefix separated by\
-                        commas (,). Example: powerMonitor,suiteCore,network"
+                        help="Get logs based on its name prefix.\
+                        Example: powerMonitor*,suiteCore,network"
                         )
 
     parser.add_argument('--log-dir',
@@ -155,3 +147,4 @@ if __name__ == '__main__':
         args.log_dir,
         args.log_prefix,
         args.output_name)
+    log_parser.get_logs()
